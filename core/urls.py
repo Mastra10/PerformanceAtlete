a4931ca50573f0f0f1aa95f6e492768935e56912
Mastra10 <andrea.mastrapasqua@gmail.com>
2026-02-10 23:26:08 +0100

    Agg commands sync 86

--- core/urls.py
+++ core/urls.py
@@ -67,6 +67,7 @@ urlpatterns = [
     path('allenamenti/elimina/<int:pk>/', views.elimina_allenamento, name='elimina_allenamento'),
     path('notifica-letta/<int:pk>/', views.segna_notifica_letta, name='segna_notifica_letta'),
     path('allenamenti/ics/<int:pk>/', views.download_allenamento_ics, name='download_allenamento_ics'),
+    path('allenamenti/gpx/<int:pk>/', views.download_allenamento_gpx, name='download_allenamento_gpx'),
 ]
 
 
