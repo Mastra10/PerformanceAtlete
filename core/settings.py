8ffcdc6925329bc1705998ab37bdfe45694c8ee4
Mastra10 <andrea.mastrapasqua@gmail.com>
2026-04-14 09:44:33 +0200

    Remove hardcoded SECRET_KEY from settings

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5i9fyk&@_^8+2)@05u6l85b(00%&buy-^e+$@#h=5+_!ds)&=s')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
