81dc247d5d939a850347855f1515f782de58b23a
Mastra10 <andrea.mastrapasqua@gmail.com>
2026-09-20 19:14:49 +0200

    new_8

--- squadra_calcio/views.py
+++ squadra_calcio/views.py
@@ -695,7 +695,7 @@ def api_salva_token_fcm(request):
 
 @csrf_exempt
 def api_check_update(request):
-    LATEST_VERSION = "1.0.3" 
+    LATEST_VERSION = "1.0.4" 
     
     DOWNLOAD_URL = "https://performance-atlete.freeddns.org/static/fraore_lab_update.apk"
     
